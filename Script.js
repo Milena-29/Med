6622ffe49a7e59a5026f6c5e49a52b1c68d5a738
Milena-29 <milena.lopnascimento@gmail.com>
2025-10-27 09:20:54 -0300

    Delete Script.js

--- Script.js
+++
@@ -1,7 +0,0 @@
-const btnTop = document.getElementById('btnTop');
-
- window.onscroll = function () {
-
- if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) { 
-btnTop.style.display = "block"; } 
-else { btnTop.style.display = "none"; } }; btnTop.addEventListener('click', () => { window.scrollTo({ top: 0, behavior: 'smooth' }); });
