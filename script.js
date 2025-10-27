fd8696c352af05b292e8c812734be2781acaf96a
Milena-29 <milena.lopnascimento@gmail.com>
2025-10-27 09:17:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnTop = document.getElementById('btnTop');
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+};
+
+btnTop.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
